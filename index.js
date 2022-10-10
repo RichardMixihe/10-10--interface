08365ca00bf83b71ca4dfdde6d72f4cfc492ffad
Richard <richardmixihe@gmail.com>
2022-10-10 15:13:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -16,6 +16,24 @@ app.get('/api/getbooks', (req, res) => {
     }
   })
 })
+// 写接口之前，加入下面两行代码，目的是为了接收客户端提交的数据
+app.use(express.json()) // 接收JSON格式的数据
+app.use(express.urlencoded({ extended: true })) // 接收查询字符串格式的数据
+
+app.post('/api/addbook',(req,res)=>{
+    req.body.id = Date.now()
+    readFile(books,'utf-8',(err,data)=>{
+        if(err) throw err;
+        let arr = JSON.parse(data)
+        arr.push(req.body)
+            if(err){
+                res.send({status:1,message:'添加失败'})
+            }else{
+                res.send({status:0,message:'添加成功'})
+            }
+    })
+    res.send({status:0,message:'添加成功'})
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
